8d2c68d321ae38806fbf3a67f80d6d88959a9917
unknown <kubira12@gmail.com>
2022-01-23 16:33:58 +0900

    javascriptを学ぶ

---
+++ html/sample/java.js
@@ -0,0 +1,22 @@
+let apple = 160;
+let grape= 160;
+
+// 条件分岐
+// if (apple < grape) {
+//   alert ("ブドウよりリンゴのほうが安い");
+// } else if (apple == grape) {
+//   alert ("ブドウとリンゴが同じ値段");
+// }else{
+//   alert("ブドウのほうがリンゴより高い");
+// }
+
+// 繰り返し、何回で1000を超えるか
+// let max = 1000;
+// let num = 1;
+// let count = 0;
+// while (num < max){
+//  num = num * 2;
+//  count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
